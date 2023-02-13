4e5b2f245bb1f2fedc66a4850d321dd7d9f717d7
f-hafner <f.hafner@esciencecenter.nl>
2023-02-13 14:00:39 +0100

    reorganize pipeline

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rebl_coref
 Use REBL with development version of REL for coref
+
+
+
+# use conda environment, development version of REL, and changed version of REBL in local folder. ie: 
+# activate conda ./env; pip install ../REBL/.; pip install -e ../REL/.
+    # try to change this to use my REL fork in the requirements
+
+
+# this is not necessary, the input file for the next step is already available 
+# python -m rebl.md.mention_detection \
+#     --in_file "../data/msmarco/msmarco_doc_00_5k.gz" \
+#     --out_file "output/md.parquet" 
+
+# pip install ../REBL/.
+# pip install -e ../REL/.
--- pipeline.sh
+++ pipeline.sh
@@ -1,63 +1,36 @@
 #!/bin/bash
 
-# use conda environment, development version of REL, and changed version of REBL in local folder. ie: 
-# activate conda ./env; pip install ../REBL/.; pip install -e ../REL/.
-    # try to change this to use my REL fork in the requirements
+# TODO: create all the necessary directories 
+# TODO: activate conda environment before running this 
 
 
-# this is not necessary, the input file for the next step is already available 
-# python -m rebl.md.mention_detection \
-#     --in_file "../data/msmarco/msmarco_doc_00_5k.gz" \
-#     --out_file "output/md.parquet" 
+# Set parameters
+DATA_URL="/var/scratch/fhafner/rel_data/"
+COREF_OPTIONS=("all" "off" "lsh")
+NDOCS=1000
 
-# pip install ../REBL/.
-# pip install -e ../REL/.
+MDFILE="${DATA_URL}/msmarco/sample_1k_longdocs.parquet"
+SOURCEFILE="${DATA_URL}/msmarco/sample_1k_longdocs.gz"
 
-# ## Effectiveness: use the batch disambiguation
+# Run 
+for option in ${COREF_OPTIONS[@]}; do
+    filename="ed_coref_${option}"
+    logfile_ed="logs/${filename}.log"
+    logfile_timing="logs/timing_${filename}.log"
+    outfile="${DATA_URL}/msmarco/predictions/${filename}.log"
 
-# run ED with different coref options 
-MDFILE="/var/scratch/fhafner/rel_data/msmarco/sample_1k_longdocs.parquet"
-SOURCEFILE="/var/scratch/fhafner/rel_data/msmarco/sample_1k_longdocs.gz"
-URL="/var/scratch/fhafner/rel_data/"
-
-python3 -m rebl.ed.entity_disambiguation \
-    --md_file $MDFILE  \
-    --fields title headings body \
-    --source_file $SOURCEFILE \
-    --out_file /var/scratch/fhafner/rel_data/msmarco/predictions/ed_coref_all.parquet \
-    --base_url $URL \
-    --wiki_version wiki_2019 \
-    --identifier docid \
-    --search_corefs "all" \
-    &> ed_coref_all.log
-
-python3 -m rebl.ed.entity_disambiguation \
-    --md_file $MDFILE \
-    --fields title headings body \
-    --source_file $SOURCEFILE \
-    --out_file /var/scratch/fhafner/rel_data/msmarco/predictions/ed_coref_lsh.parquet \
-    --base_url $URL \
-    --wiki_version wiki_2019 \
-    --identifier docid \
-    --search_corefs "lsh" \
-    &> ed_coref_lsh.log
-
-python3 -m rebl.ed.entity_disambiguation \
-    --md_file $MDFILE  \
-    --fields title headings body \
-    --source_file $SOURCEFILE \
-    --out_file /var/scratch/fhafner/rel_data/msmarco/predictions/ed_coref_off.parquet \
-    --base_url $URL \
-    --wiki_version wiki_2019 \
-    --identifier docid \
-    --search_corefs "off" \
-    &> ed_coref_off.log
-
-
-# ## Efficiency: disambiguate by document 
-python3 msmarco_timing.py --n_docs 1000 --search_corefs "all" &> timing_ed_coref_all.log
-python3 msmarco_timing.py --n_docs 1000 --search_corefs "lsh" &> timing_ed_coref_lsh.log
-python3 msmarco_timing.py --n_docs 1000 --search_corefs "off" &> timing_ed_coref_off.log
-
-
-# python3 msmarco_largedoc_memory.py --n_docs 5000 --search_corefs "lsh" 
+    # ## Effectiveness: use the batch disambiguation
+    python -m rebl.ed.entity_disambiguation \
+        --md_file $MDFILE  \
+        --fields title headings body \
+        --source_file $SOURCEFILE \
+        --out_file $outfile \
+        --base_url $DATA_URL \
+        --wiki_version wiki_2019 \
+        --identifier docid \
+        --search_corefs $option \
+        &> $logfile_ed
+    
+    # ## Efficiency: disambiguate by document 
+    python msmarco_timing.py --n_docs $NDOCS --search_corefs $option &> $logfile_timing
+done 
